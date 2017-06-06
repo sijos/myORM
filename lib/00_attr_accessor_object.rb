c7a7732263ebd32ea5d63984c7430c559ef919a5
Scott Snyder <ssny88@gmail.com>
2017-06-06 11:24:50 -0500

    complete attr_accessor phase 0

--- lib/00_attr_accessor_object.rb
+++ lib/00_attr_accessor_object.rb
@@ -1,5 +1,13 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |var|
+      define_method var do
+        self.instance_variable_get("@#{var}")
+      end
+
+      define_method("#{var}=") do |value|
+        self.instance_variable_set("@#{var}", value)
+      end
+    end
   end
 end
